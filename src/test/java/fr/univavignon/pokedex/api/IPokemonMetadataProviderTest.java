b6af7184ff75c70bc86e1d4c15abef7512290c73
K-Lovelace <gael.cuminal@hotmail.fr>
2017-03-25 15:41:47 +0100

    first test passing

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -6,29 +6,29 @@ import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-/**
- * Created by lovelacez on 3/6/17.
- */
 public final class IPokemonMetadataProviderTest extends TestCase {
-
-    @Mock
-    private static IPokemonMetadataProvider pokemonMetadataProvider;
+    
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
     
+    @Mock
+    private static IPokemonMetadataProvider pokemonMetadataProvider;
+    
     @Before
     public void setUp() throws PokedexException {
+        MockitoAnnotations.initMocks(this);
         when(pokemonMetadataProvider.getPokemonMetadata(0)).thenReturn(new PokemonMetadata(0, "Bulbizarre", 126, 126, 90));
     }
     
     @Test
     public void testGetPokemonMetadata() throws PokedexException {
-        assertEquals(pokemonMetadataProvider.getPokemonMetadata(0), new PokemonMetadata(0, "Bulbizarre", 126, 126, 90));
+        assertEquals("Bulbizarre", pokemonMetadataProvider.getPokemonMetadata(0).getName());
     }
 }
