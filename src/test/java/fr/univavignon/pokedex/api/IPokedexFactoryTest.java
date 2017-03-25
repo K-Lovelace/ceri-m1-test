96f67e888a09bc7dcac25136dfc6e198c1afc1ba
K-Lovelace <gael.cuminal@hotmail.fr>
2017-03-25 16:08:25 +0100

    second test passing

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,7 +1,44 @@
 package fr.univavignon.pokedex.api;
 
-/**
- * Created by lovelacez on 3/6/17.
- */
-public class IPokedexFactoryTest {
+import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public final class IPokedexFactoryTest extends TestCase {
+    
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+    
+    @Mock
+    private static IPokedexFactory pokedexFactory;
+    
+    @Mock
+    private static IPokemonMetadataProvider pokemonMetadataProvider;
+    
+    @Mock
+    private static IPokemonFactory pokemonFactory;
+    
+    @Mock
+    private static IPokedex pokedex;
+    
+    @Before
+    public void setUp() throws PokedexException {
+        MockitoAnnotations.initMocks(this);
+        IPokedex mockPokedex = mock(IPokedex.class);
+        when(pokedexFactory.createPokedex(pokemonMetadataProvider, pokemonFactory))
+                .thenReturn(mockPokedex);
+    }
+    
+    @Test
+    public void testGetPokemonMetadata() throws PokedexException {
+        assertEquals(pokedex.size(), pokedexFactory.createPokedex(pokemonMetadataProvider, pokemonFactory).size());
+    }
 }
